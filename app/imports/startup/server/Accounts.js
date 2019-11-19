e3c6f67f372541b4a4c8bff47c8301651d817ec2
jayryanj <43054912+jayryanj@users.noreply.github.com>
2019-11-19 11:41:26 -1000

    Created default profiles for default users

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
+import { Profiles } from '../../api/profile/Profile';
+import swal from 'sweetalert';
 
 /* eslint-disable no-console */
 
@@ -23,11 +25,17 @@ function createUser(email, password, firstName, lastName, studentId, campus, rol
   }
 }
 
+function createProfile(firstName, lastName, studentId, campus, email) {
+  console.log(` Creating profile for user ${email}`);
+  Profiles.insert({ firstName, lastName, studentId, campus, owner: email });
+}
+
 /** When running app for first time, pass a settings file to set up a default user account. */
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
     Meteor.settings.defaultAccounts.map(({ email, password, firstName, lastName, studentId, campus,role }) => createUser(email, password, firstName, lastName, studentId, campus, role));
+    Meteor.settings.defaultProfiles.map(({firstName, lastName, studentId, campus, owner }) => createProfile(firstName, lastName, studentId, campus, owner));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
