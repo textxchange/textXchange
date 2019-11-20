45e8ae42f7335c7796f8157737241fd95db9b764
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2019-11-19 21:50:42 -1000

    Discover Page added

--- app/imports/ui/pages/Discover.jsx
+++ app/imports/ui/pages/Discover.jsx
@@ -1,69 +1,47 @@
 import React from 'react';
-import { Card, Header, Container, Icon, Image } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { Card, Header, Container } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import Book from '../components/Book';
+import { Books } from '../../api/book/Book';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Discover extends React.Component {
   render() {
+    const book1 = [
+      {
+        ISBN: '12345',
+        title: 'Book',
+        author: 'Jeff Wong',
+        datePublished: new Date(),
+        description: 'This is a book',
+        cost: '13',
+        owner: 'john@foo.com',
+        class: 'ICS314',
+        image: 'https://media.discordapp.net/attachments/641715894984245258/646252553176219668/textXchange_Logo_4.png',
+        datePosted: new Date(),
+        condition: 'good',
+      },
+      {
+        ISBN: '12345',
+        title: 'Book',
+        author: 'Jeff Wong',
+        datePublished: new Date(),
+        description: 'This is a book',
+        cost: '13',
+        owner: 'john@foo.com',
+        class: 'ICS314',
+        image: 'https://media.discordapp.net/attachments/641715894984245258/646252553176219668/textXchange_Logo_4.png',
+        datePosted: new Date(),
+        condition: 'good',
+      },
+    ];
     return (
         <Container>
           <Header as='h1' textAlign="center" inverted>Browse</Header>
           <Card.Group centered>
-            <Card>
-              <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>Matthew</Card.Header>
-                <Card.Meta>
-                  <span className='date'>Joined in 2015</span>
-                </Card.Meta>
-                <Card.Description>
-                  Matthew is a musician living in Nashville.
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name='user' />
-                  22 Friends
-                </a>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>Matthew</Card.Header>
-                <Card.Meta>
-                  <span className='date'>Joined in 2015</span>
-                </Card.Meta>
-                <Card.Description>
-                  Matthew is a musician living in Nashville.
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name='user' />
-                  22 Friends
-                </a>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>Matthew</Card.Header>
-                <Card.Meta>
-                  <span className='date'>Joined in 2015</span>
-                </Card.Meta>
-                <Card.Description>
-                  Matthew is a musician living in Nashville.
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name='user' />
-                  22 Friends
-                </a>
-              </Card.Content>
-            </Card>
+            {book1.map((book, index) => <Book key={index} book={book}/>)}
           </Card.Group>
         </Container>
     );
@@ -72,8 +50,16 @@ class Discover extends React.Component {
 
 /** Require a document to be passed to this component. */
 Discover.propTypes = {
-  stuff: PropTypes.object.isRequired,
+  books: PropTypes.array.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(Discover);
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe('Book');
+  return {
+    books: Books.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(Discover);
