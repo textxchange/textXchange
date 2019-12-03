b17dee8d5be9676fa40b65829924d3ffc9b4936d
jayryanj <43054912+jayryanj@users.noreply.github.com>
2019-12-03 01:00:06 -1000

    Added default books
    Created several default books that are initialized upon startup.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Books } from '../../api/book/Book';
 
 /* eslint-disable no-console */
 
@@ -9,6 +10,11 @@ function addData(data) {
   Stuffs.insert(data);
 }
 
+function addBook(book) {
+  console.log(`  Adding: ${book.title} (${book.owner})`);
+  Books.insert(book);
+}
+
 /** Initialize the collection if empty. */
 if (Stuffs.find().count() === 0) {
   if (Meteor.settings.defaultData) {
@@ -16,3 +22,17 @@ if (Stuffs.find().count() === 0) {
     Meteor.settings.defaultData.map(data => addData(data));
   }
 }
+
+/** Initialize the collection if empty. */
+if (Books.find().count() === 0) {
+  if (Meteor.settings.defaultBooks) {
+    console.log('Creating default books.');
+    Meteor.settings.defaultBooks.map(book => addBook(book));
+  }
+}
+
+
+
+
+
+
