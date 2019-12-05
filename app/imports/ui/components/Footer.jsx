2ab745b657970c81032ae9fedb806354d424c444
Nadine Alcantara <nadine-alcantara@users.noreply.github.com>
2019-12-05 13:14:20 -1000

    Update Footer.jsx

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '1em' , marginTop: '5em', color: 'white' };
+    const divStyle = { paddingTop: '1em', color: 'black' };
     return (
-        <div style={{ background: 'black' }}>
+        <div style={{ background: '#c8e1cc' }}>
           <footer>
             <div style={divStyle} className="ui center aligned container">
               <hr />
