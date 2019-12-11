dc17fad51941947c39e4f6587ce49a7b8ddf95c9
Nathan Zenger <43054888+nzenger@users.noreply.github.com>
2019-12-11 00:46:09 -1000

    Finished  buy book email notification

--- app/imports/ui/pages/BuyBook.jsx
+++ app/imports/ui/pages/BuyBook.jsx
@@ -14,27 +14,37 @@ import ProfileBook from "../components/ProfileBook";
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon";
 import Modal from "semantic-ui-react/dist/commonjs/modules/Modal";
 import { Profiles } from "/imports/api/profile/Profile";
+import { Email } from "meteor/email";
+import Confirm from "semantic-ui-react/dist/commonjs/addons/Confirm";
 
 /** Create a schema to specify the structure of the data to appear in the form. */
 const right = { float: "right" };
 const noPadding = { paddingBottom: "7px" };
 
-Meteor.methods({
-  sendEmail(to, from, subject, text) {
-    // Make sure that all arguments are strings.
-    check([to, from, subject, text], [String]);
-
-    // Let other method calls from the same client start running, without
-    // waiting for the email sending to complete.
-    this.unblock();
-
-    Email.send({ to, from, subject, text });
-  }
-});
+let emailSent = false;
 
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class BuyBook extends React.Component {
+  state = { open: false };
+
+  open = () => this.setState({ open: true });
+  close = () => this.setState({ open: false });
+
+  state = { modalOpen: false };
+
+  handleOpen = () => this.setState({ modalOpen: true });
+
+  handleClose = () => this.setState({ modalOpen: false });
+
+  plsEmail(email, book, image, buyerName) {
+    if (emailSent == false) {
+      Meteor.call("buyEmail", email, book, image, buyerName);
+      emailSent = true;
+    }
+    this.setState({ modalOpen: true });
+    this.setState({ open: false });
+  }
 
   constructor(props) {
     super(props);
@@ -54,56 +64,75 @@ class BuyBook extends React.Component {
   }
 
   renderPage() {
+    const { open } = this.state;
     return (
         <Container>
           <Header as='h1' textAlign="center" inverted>Buy Book</Header>
-          <Card fluid>
-            <Card.Group centered>
-              <Card color='red' image={this.select.image}/>
-            </Card.Group>
-            <Card.Content>
-              <Card.Header>{this.select.title}</Card.Header>
-              <Card.Meta>{this.select.author}</Card.Meta>
-              <Card.Description> {this.select.description} </Card.Description>
-              <Label tag floated='right'>
-                {this.select.classUsed}
-              </Label>
-            </Card.Content>
-            <Card.Content style={noPadding}>
-              <Card.Meta>
-                {/* eslint-disable-next-line max-len */}
-                <Image className='profile-pic' floated='left'
-                    /* eslint-disable-next-line max-len */
-                       src='https://media.discordapp.net/attachments/641715894984245258/646252553176219668/textXchange_Logo_4.png'/>
-                $ {this.select.cost}
-                <span style={right}> Posted {this.select.datePosted.toLocaleDateString()} </span>
-              </Card.Meta>
-            </Card.Content>
-            <Card.Content extra>
-              <Modal
-                  trigger={<Button basic color='green' content='Contact Seller for Purchase' onClick={Meteor.call(
-                      "sendEmail",
-                      "textXchange.team@gmail.com",
-                      this.select.owner,
-                      "textXchange, Buyer Notification!",
-                      "Hello " + this.select.owner + "," +
-                      this.props.profile[0].firstName + " " +
-                      this.props.profile[0].lastName +
-                      " would like to purchase your book: " +
-                      this.select.title + "please contact them via email at : " +
-                      this.select.owner + " to setup an exchange." + "    -textXchange"
-                  )}/>}
-                  header='Exchange Initiated'
-                  content='The seller will be notified of your intent to purchase and will contact you!'
-                  actions={[{ key: "done", content: "Got it", positive: true }]}
-              />
-              <span style={right}> Owned by {this.select.owner}</span>
-            </Card.Content>
-          </Card>
+          <Card.Group itemsPerRow={2}>
+            <Card>
+              <Image centered src={this.select.image} size='medium'/>
+            </Card>
+            <Card>
+              <Card.Content>
+                <Card.Header>{this.select.title}</Card.Header>
+                <Card.Meta>{this.select.author}</Card.Meta>
+                <Card.Description> {this.select.description} </Card.Description>
+                <Label tag floated='right'>
+                  {this.select.classUsed}
+                </Label>
+              </Card.Content>
+              <Card.Content style={noPadding}>
+                <Card.Meta>
+                  {/* eslint-disable-next-line max-len */}
+                  <Image className='profile-pic' floated='left'
+                      /* eslint-disable-next-line max-len */
+                         src='https://media.discordapp.net/attachments/641715894984245258/646252553176219668/textXchange_Logo_4.png'/>
+                  $ {this.select.cost}
+                  <span style={right}> Posted {this.select.datePosted.toLocaleDateString()} </span>
+                </Card.Meta>
+              </Card.Content>
+              <Card.Content extra>
+                <Button centered basic color='green' onClick={this.open}>
+                  Contact Seller For Purchase
+                </Button>
+                <Confirm
+                    header='Purchase Confirmation'
+                    content='The seller of this book will be notified of your intent to purchase and will contact you via email.'
+                    cancelButton='Cancel Purchase'
+                    confirmButton="Confirm Purchase"
+                    open={this.state.open}
+                    onCancel={this.close}
+                    onConfirm={() => this.plsEmail(
+                        this.select.owner,
+                        this.select.title,
+                        this.select.image,
+                        this.props.profile[0].firstName
+                    )}/>
+
+                <Modal
+                    open={this.state.modalOpen}
+                    onClose={this.handleClose}
+                    basic
+                    size='small'
+                >
+                  <Header icon='checkmark' content='Purchase Confirmed!'/>
+                  <Modal.Content>
+                    <h3>Email has been sent to seller</h3>
+                  </Modal.Content>
+                  <Modal.Actions>
+                    <Button color='green' onClick={this.handleClose} inverted>
+                      Got it
+                    </Button>
+                  </Modal.Actions>
+                </Modal>
+              </Card.Content>
+            </Card>
+          </Card.Group>
         </Container>
     );
   }
 }
+
 
 /** Require a document to be passed to this component. */
 BuyBook.propTypes = {
