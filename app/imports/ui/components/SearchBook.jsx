cadc53554ded58112a8d9910305cf2d541e6d147
jeff-yc-wong <42687471+jeff-yc-wong@users.noreply.github.com>
2019-12-14 19:24:48 -1000

    Update SearchBook.jsx

--- app/imports/ui/components/SearchBook.jsx
+++ app/imports/ui/components/SearchBook.jsx
@@ -8,110 +8,110 @@ import { Books } from '../../api/book/Book';
 
 class SearchBook extends React.Component {
 
-  render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = { param: 'title', refer: false, search: '' };
-  }
-
-  submit() {
-    console.log(this.state.search);
-    if (this.state.search !== '') {
-      this.setState({ refer: true });
+    render() {
+        return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
     }
-  }
 
-  handleFormSubmit = () => {
-    const searchValue = this.state.query;
-    if (searchValue) {
-      Meteor.subscribe('BookPublic');
-      return {
-        books: Books.find({ name: searchValue }).fetch(),
-      };
+    constructor(props) {
+        super(props);
+        this.state = { param: 'title', refer: false, search: '' };
     }
-    return null;
-  }
 
-  handleChange = (e) => {
-    this.setState({ search: e.target.value });
-  }
-
-  handleInputChange = (e) => {
-    if (e.key === 'Enter') {
-      if (e.target.value !== '') {
-        this.setState({ refer: true, search: e.target.value });
-      }
+    submit() {
+        console.log(this.state.search);
+        if (this.state.search !== '') {
+            this.setState({ refer: true });
+        }
     }
-  }
 
-  handleSelectChange = (e) => {
-    this.setState({ param: e.target.value });
-  }
+    handleFormSubmit = () => {
+        const searchValue = this.state.query;
+        if (searchValue) {
+            Meteor.subscribe('BookPublic');
+            return {
+                books: Books.find({ name: searchValue }).fetch(),
+            };
+        }
+        return null;
+    }
 
-  searchItems = (book) => {
-    if (book.title.toLowerCase().includes(this.state.query.toLowerCase())) {
-      return true;
+    handleChange = (e) => {
+        this.setState({ search: e.target.value });
     }
-    if (book.classUsed.toLowerCase().includes(this.state.query.toLowerCase())) {
-      return true;
-    }
-    if (this.state.query === '') {
-      return true;
-    }
-    return false;
-  }
 
-  renderPage() {
-    const options = [
-      { key: 'title', text: 'Title', value: 'title' },
-      { key: 'class', text: 'Class', value: 'class' },
-      { key: 'author', text: 'Author', value: 'author' },
-    ];
-
-    if (this.state.refer === true && this.state.search !== '') {
-      return <Redirect
-          to={{ pathname: '/discover', state: { search: this.state.search, param: this.state.param } }}/>;
+    handleInputChange = (e) => {
+        if (e.key === 'Enter') {
+            if (e.target.value !== '') {
+                this.setState({ refer: true, search: e.target.value });
+            }
+        }
     }
-    return (
-        <div className='searchBar'>
-          <Input
-              fluid
-              icon
-              iconPosition='left'
-              placeholder='Search for a book now!'
-          >
-            <i className='book icon'/>
-            <input style={{ borderRadius: '7000px 0px 0px 7000px' }}
-                   onKeyDown={this.handleInputChange}
-                   onChange={this.handleChange}/>
-            <Select style={{ borderRadius: '0px 0px 0px 0px' }}
-                    compact options={options} defaultValue='title' onChange={this.handleSelectChange}/>
-            <Button type='submit'
+
+    handleSelectChange = (e) => {
+        this.setState({ param: e.target.value });
+    }
+
+    searchItems = (book) => {
+        if (book.title.toLowerCase().includes(this.state.query.toLowerCase())) {
+            return true;
+        }
+        if (book.classUsed.toLowerCase().includes(this.state.query.toLowerCase())) {
+            return true;
+        }
+        if (this.state.query === '') {
+            return true;
+        }
+        return false;
+    }
+
+    renderPage() {
+        const options = [
+            { key: 'title', text: 'Title', value: 'title' },
+            { key: 'class', text: 'Class', value: 'class' },
+            { key: 'author', text: 'Author', value: 'author' },
+        ];
+
+        if (this.state.refer === true && this.state.search !== '') {
+            return <Redirect
+                to={{ pathname: '/discover', state: { search: this.state.search, param: this.state.param } }}/>;
+        }
+        return (
+            <div className='searchBar'>
+                <Input
+                    fluid
                     icon
-                    style={{ borderRadius: '0px 7000px 7000px 0px' }}
-                    onClick={this.submit.bind(this)}>
-              <Icon name='search' />
-            </Button>
-          </Input>
-        </div>
-    );
+                    iconPosition='left'
+                    placeholder='Search for a book now!'
+                >
+                    <i className='book icon'/>
+                    <input style={{ borderRadius: '7000px 0px 0px 7000px' }}
+                           onKeyDown={this.handleInputChange}
+                           onChange={this.handleChange}/>
+                    <Select style={{ borderRadius: '0px 0px 0px 0px' }}
+                            compact options={options} defaultValue='title' onChange={this.handleSelectChange}/>
+                    <Button type='submit'
+                            icon
+                            style={{ borderRadius: '0px 7000px 7000px 0px' }}
+                            onClick={this.submit.bind(this)}>
+                        <Icon name='search'/>
+                    </Button>
+                </Input>
+            </div>
+        );
 
-  }
+    }
 }
 
 SearchBook.propTypes = {
-  books: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
+    books: PropTypes.array.isRequired,
+    ready: PropTypes.bool.isRequired,
 };
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('BookPublic');
-  return {
-    books: Books.find({}).fetch(),
-    ready: subscription.ready(),
-  };
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe('BookPublic');
+    return {
+        books: Books.find({}).fetch(),
+        ready: subscription.ready(),
+    };
 })(SearchBook);
