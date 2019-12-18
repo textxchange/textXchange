ba089b2d747176ddb1c03bf15ba4ed52ea9166be
Nadine Alcantara <nadine-alcantara@users.noreply.github.com>
2019-12-17 18:38:12 -1000

    Prevented negative cost

--- app/imports/ui/pages/AddBook.jsx
+++ app/imports/ui/pages/AddBook.jsx
@@ -20,7 +20,10 @@ const formSchema = new SimpleSchema({
   author: String,
   yearPublished: Date,
   description: String,
-  cost: Number,
+  cost: {
+    type: Number,
+    min: 0,
+  },
   classUsed: String,
   datePosted: { type: Date, defaultValue: new Date() },
   condition: {
@@ -74,7 +77,7 @@ class AddBook extends React.Component {
                     'What does this book cover? Was it helpful? Was it mandatory?
                 Now is your chance to tell the community!'/>
                 <Form.Group widths={'equal'}>
-                  <NumField iconLeft='dollar' name='cost' decimal={true} min='0'/>
+                  <NumField iconLeft='dollar' name='cost' decimal={true} min='0' />
                   <Popup
                       trigger={<TextField name='classUsed'/>}
                       header='Use the course&apos;s abbreviated name and number'
